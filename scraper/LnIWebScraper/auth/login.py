150ccb991029c5f9afb7e8c176d6206ab4ca24bd
DJ-Addy <achermouh808@gmail.com>
2025-03-24 23:17:36 -0500

    modified

--- scraper/LnIWebScraper/auth/login.py
+++ scraper/LnIWebScraper/auth/login.py
@@ -5,8 +5,6 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
 }
 
-#email:pattersonbroski@gmail.com
-#password:Coffee!1425
 
 def login(session, email, password):
     payload = {
